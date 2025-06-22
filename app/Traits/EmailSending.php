121993ce657c0c0e9b14882b31d45f39ba58c5ff
Hafiz Irfan <hafizirfan8078@gmail.com>
2025-06-22 22:43:30 +0500

    revert mail

--- app/Traits/EmailSending.php
+++ app/Traits/EmailSending.php
@@ -24,7 +24,7 @@ trait EmailSending
         ])->post('https://api.brevo.com/v3/smtp/email', [
             'sender' => [
                 'name' => 'roohulquran',
-                'email' => 'hafizirfan8078@gmail.com',
+                'email' => 'hafizirfan11123@gmail.com',
             ],
             'to' => [
                 ['email' => $to],
