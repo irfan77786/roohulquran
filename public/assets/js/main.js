3c2de09c8e8fa96b18b0a139c74aaa03e87a1dc4
Hafiz Irfan <hafizirfan8078@gmail.com>
2025-09-17 19:38:22 +0500

    new chanegs

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -96,12 +96,14 @@
      * Animation on scroll function and init
      */
     function aosInit() {
-        AOS.init({
-            duration: 600,
-            easing: "ease-in-out",
-            once: true,
-            mirror: false,
-        });
+        if (window.AOS && typeof AOS.init === "function") {
+            AOS.init({
+                duration: 600,
+                easing: "ease-in-out",
+                once: true,
+                mirror: false,
+            });
+        }
     }
     window.addEventListener("load", aosInit);
 
