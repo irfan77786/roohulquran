35049a067ee4b5bc99532cb2b6e988dee3368855
Hafiz Irfan <hafizirfan8078@gmail.com>
2025-10-07 14:25:27 +0500

    again aded

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -31,8 +31,8 @@
         <link rel="preconnect" href="https://img.youtube.com" crossorigin>
 
         <!-- Defer Bootstrap CSS to reduce render-blocking -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" media="print"
+            onload="this.media='all'">
 
         <!-- Preload site CSS; load non-blocking to reduce render-blocking time -->
         <link rel="preload" href="{{ asset('assets/css/main.css') }}" as="style">
@@ -48,7 +48,7 @@
 
 
         <noscript>
-
+            <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
             <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
             <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
             <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
@@ -309,9 +309,7 @@
             </svg>
         </span>
     </a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    <script defer src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script defer src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
     {{-- <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script> --}}
     <script defer src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
