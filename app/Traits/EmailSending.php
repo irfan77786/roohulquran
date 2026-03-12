53495ca4eb2cca48e43a8c1e184bef6300c3891e
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-03-13 01:49:20 +0500

    send mail

--- app/Traits/EmailSending.php
+++ app/Traits/EmailSending.php
@@ -2,45 +2,21 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Http;
 use Exception;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 trait EmailSending
 {
     /**
      * @throws Exception
      */
-
-     public function sendEmail(string $to, string $subject, string $htmlContent): void
-     {
- 
-        $apiKey = config('brevo.api_key');
-         $response = Http::withHeaders([
-            'api-key' => $apiKey,
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json',
-        ])->post('https://api.brevo.com/v3/smtp/email', [
-            'sender' => [
-                'name' => 'roohulquran',
-                'email' => 'hafizirfan11123@gmail.com',
-            ],
-            'to' => [
-                ['email' => $to],
-            ],
-            'subject' => $subject,
-            'htmlContent' => $htmlContent,
-        ]);
-        
-        // Log or dd the response
-        if ($response->failed()) {
-            Log::error('Brevo Email Error', [
-                'to' => $to,
-                'subject' => $subject,
-                'response' => $response->body()
-            ]);
-        
-        }
-     }
+    public function sendEmail(string $to, string $subject, string $htmlContent): void
+    {
+        // Uses Laravel's Mail facade, which will use your Gmail SMTP
+        Mail::send([], [], function ($message) use ($to, $subject, $htmlContent) {
+            $message->to($to)
+                ->subject($subject)
+                ->html($htmlContent);
+        });
+    }
 }
