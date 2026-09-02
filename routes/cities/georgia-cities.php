af2d331438d27d2e974407a29608c656d0b6c784
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-09-03 04:10:25 +0500

    temp urls

---
+++ routes/cities/georgia-cities.php
@@ -0,0 +1,444 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/abasha/quran-academy-abasha-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'abasha')
+    ->defaults('state', 'georgia');
+
+Route::get('/adigeni/quran-academy-adigeni-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'adigeni')
+    ->defaults('state', 'georgia');
+
+Route::get('/adigeni-municipality/quran-academy-adigeni-municipality-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'adigeni-municipality')
+    ->defaults('state', 'georgia');
+
+Route::get('/agara/quran-academy-agara-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'agara')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhaldaba/quran-academy-akhaldaba-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhaldaba')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhaldaba-1/quran-academy-akhaldaba-1-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhaldaba-1')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhalgori/quran-academy-akhalgori-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhalgori')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhalkalaki/quran-academy-akhalkalaki-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhalkalaki')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhaltsikhe/quran-academy-akhaltsikhe-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhaltsikhe')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhaltsikhis-munitsipaliteti/quran-academy-akhaltsikhis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhaltsikhis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/akhmeta/quran-academy-akhmeta-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'akhmeta')
+    ->defaults('state', 'georgia');
+
+Route::get('/ambrolauri/quran-academy-ambrolauri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ambrolauri')
+    ->defaults('state', 'georgia');
+
+Route::get('/ambrolauris-munitsipaliteti/quran-academy-ambrolauris-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ambrolauris-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/aspindza/quran-academy-aspindza-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'aspindza')
+    ->defaults('state', 'georgia');
+
+Route::get('/aspindzis-munitsipaliteti/quran-academy-aspindzis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'aspindzis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/baghdatis-munitsipaliteti/quran-academy-baghdatis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'baghdatis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/bakuriani/quran-academy-bakuriani-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'bakuriani')
+    ->defaults('state', 'georgia');
+
+Route::get('/batumi/quran-academy-batumi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'batumi')
+    ->defaults('state', 'georgia');
+
+Route::get('/bichvinta/quran-academy-bichvinta-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'bichvinta')
+    ->defaults('state', 'georgia');
+
+Route::get('/bolnisi/quran-academy-bolnisi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'bolnisi')
+    ->defaults('state', 'georgia');
+
+Route::get('/bolnisis-munitsipaliteti/quran-academy-bolnisis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'bolnisis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/borjomi/quran-academy-borjomi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'borjomi')
+    ->defaults('state', 'georgia');
+
+Route::get('/chakvi/quran-academy-chakvi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'chakvi')
+    ->defaults('state', 'georgia');
+
+Route::get('/chiatura/quran-academy-chiatura-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'chiatura')
+    ->defaults('state', 'georgia');
+
+Route::get('/didi-lilo/quran-academy-didi-lilo-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'didi-lilo')
+    ->defaults('state', 'georgia');
+
+Route::get('/dioknisi/quran-academy-dioknisi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'dioknisi')
+    ->defaults('state', 'georgia');
+
+Route::get('/dmanisis-munitsipaliteti/quran-academy-dmanisis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'dmanisis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/dranda/quran-academy-dranda-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'dranda')
+    ->defaults('state', 'georgia');
+
+Route::get('/dzegvi/quran-academy-dzegvi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'dzegvi')
+    ->defaults('state', 'georgia');
+
+Route::get('/gagra/quran-academy-gagra-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gagra')
+    ->defaults('state', 'georgia');
+
+Route::get('/gali/quran-academy-gali-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gali')
+    ->defaults('state', 'georgia');
+
+Route::get('/gantiadi/quran-academy-gantiadi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gantiadi')
+    ->defaults('state', 'georgia');
+
+Route::get('/gardabani/quran-academy-gardabani-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gardabani')
+    ->defaults('state', 'georgia');
+
+Route::get('/gardabnis-munitsipaliteti/quran-academy-gardabnis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gardabnis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/gori/quran-academy-gori-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gori')
+    ->defaults('state', 'georgia');
+
+Route::get('/goris-munitsipaliteti/quran-academy-goris-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'goris-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/gudauri/quran-academy-gudauri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gudauri')
+    ->defaults('state', 'georgia');
+
+Route::get('/gudauta/quran-academy-gudauta-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gudauta')
+    ->defaults('state', 'georgia');
+
+Route::get('/gurjaani/quran-academy-gurjaani-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'gurjaani')
+    ->defaults('state', 'georgia');
+
+Route::get('/java/quran-academy-java-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'java')
+    ->defaults('state', 'georgia');
+
+Route::get('/jvari/quran-academy-jvari-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'jvari')
+    ->defaults('state', 'georgia');
+
+Route::get('/kaspi/quran-academy-kaspi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kaspi')
+    ->defaults('state', 'georgia');
+
+Route::get('/kelasuri/quran-academy-kelasuri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kelasuri')
+    ->defaults('state', 'georgia');
+
+Route::get('/kharagauli/quran-academy-kharagauli-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kharagauli')
+    ->defaults('state', 'georgia');
+
+Route::get('/khashuri/quran-academy-khashuri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'khashuri')
+    ->defaults('state', 'georgia');
+
+Route::get('/khelvachauri/quran-academy-khelvachauri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'khelvachauri')
+    ->defaults('state', 'georgia');
+
+Route::get('/khobi/quran-academy-khobi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'khobi')
+    ->defaults('state', 'georgia');
+
+Route::get('/khoni/quran-academy-khoni-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'khoni')
+    ->defaults('state', 'georgia');
+
+Route::get('/khulo/quran-academy-khulo-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'khulo')
+    ->defaults('state', 'georgia');
+
+Route::get('/kobuleti/quran-academy-kobuleti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kobuleti')
+    ->defaults('state', 'georgia');
+
+Route::get('/kutaisi/quran-academy-kutaisi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kutaisi')
+    ->defaults('state', 'georgia');
+
+Route::get('/kveda-chkhorotsqu/quran-academy-kveda-chkhorotsqu-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kveda-chkhorotsqu')
+    ->defaults('state', 'georgia');
+
+Route::get('/kalaki-chiatura/quran-academy-kalaki-chiatura-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kalaki-chiatura')
+    ->defaults('state', 'georgia');
+
+Route::get('/kulashi/quran-academy-kulashi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'kulashi')
+    ->defaults('state', 'georgia');
+
+Route::get('/lagodekhi/quran-academy-lagodekhi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'lagodekhi')
+    ->defaults('state', 'georgia');
+
+Route::get('/lanchkhuti/quran-academy-lanchkhuti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'lanchkhuti')
+    ->defaults('state', 'georgia');
+
+Route::get('/lentekhi/quran-academy-lentekhi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'lentekhi')
+    ->defaults('state', 'georgia');
+
+Route::get('/makhinjauri/quran-academy-makhinjauri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'makhinjauri')
+    ->defaults('state', 'georgia');
+
+Route::get('/manglisi/quran-academy-manglisi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'manglisi')
+    ->defaults('state', 'georgia');
+
+Route::get('/marneuli/quran-academy-marneuli-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'marneuli')
+    ->defaults('state', 'georgia');
+
+Route::get('/marneulis-munitsipaliteti/quran-academy-marneulis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'marneulis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/martvili/quran-academy-martvili-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'martvili')
+    ->defaults('state', 'georgia');
+
+Route::get('/mestia/quran-academy-mestia-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'mestia')
+    ->defaults('state', 'georgia');
+
+Route::get('/mestiis-munitsipaliteti/quran-academy-mestiis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'mestiis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/mtskheta/quran-academy-mtskheta-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'mtskheta')
+    ->defaults('state', 'georgia');
+
+Route::get('/naghvarevi/quran-academy-naghvarevi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'naghvarevi')
+    ->defaults('state', 'georgia');
+
+Route::get('/naruja/quran-academy-naruja-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'naruja')
+    ->defaults('state', 'georgia');
+
+Route::get('/ninotsminda/quran-academy-ninotsminda-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ninotsminda')
+    ->defaults('state', 'georgia');
+
+Route::get('/ochkhamuri/quran-academy-ochkhamuri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ochkhamuri')
+    ->defaults('state', 'georgia');
+
+Route::get('/ochamchire/quran-academy-ochamchire-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ochamchire')
+    ->defaults('state', 'georgia');
+
+Route::get('/oni/quran-academy-oni-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'oni')
+    ->defaults('state', 'georgia');
+
+Route::get('/orsantia/quran-academy-orsantia-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'orsantia')
+    ->defaults('state', 'georgia');
+
+Route::get('/ozurgeti/quran-academy-ozurgeti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ozurgeti')
+    ->defaults('state', 'georgia');
+
+Route::get('/pasanauri/quran-academy-pasanauri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'pasanauri')
+    ->defaults('state', 'georgia');
+
+Route::get('/poti/quran-academy-poti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'poti')
+    ->defaults('state', 'georgia');
+
+Route::get('/primorskoe/quran-academy-primorskoe-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'primorskoe')
+    ->defaults('state', 'georgia');
+
+Route::get('/qvareli/quran-academy-qvareli-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'qvareli')
+    ->defaults('state', 'georgia');
+
+Route::get('/rustavi/quran-academy-rustavi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'rustavi')
+    ->defaults('state', 'georgia');
+
+Route::get('/sachkhere/quran-academy-sachkhere-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'sachkhere')
+    ->defaults('state', 'georgia');
+
+Route::get('/sagarejo/quran-academy-sagarejo-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'sagarejo')
+    ->defaults('state', 'georgia');
+
+Route::get('/samtredia/quran-academy-samtredia-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'samtredia')
+    ->defaults('state', 'georgia');
+
+Route::get('/senaki/quran-academy-senaki-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'senaki')
+    ->defaults('state', 'georgia');
+
+Route::get('/shorapani/quran-academy-shorapani-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'shorapani')
+    ->defaults('state', 'georgia');
+
+Route::get('/shuakhevi/quran-academy-shuakhevi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'shuakhevi')
+    ->defaults('state', 'georgia');
+
+Route::get('/sighnaghi/quran-academy-sighnaghi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'sighnaghi')
+    ->defaults('state', 'georgia');
+
+Route::get('/sokhumi/quran-academy-sokhumi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'sokhumi')
+    ->defaults('state', 'georgia');
+
+Route::get('/stantsiya-novyy-afon/quran-academy-stantsiya-novyy-afon-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'stantsiya-novyy-afon')
+    ->defaults('state', 'georgia');
+
+Route::get('/stepantsminda/quran-academy-stepantsminda-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'stepantsminda')
+    ->defaults('state', 'georgia');
+
+Route::get('/surami/quran-academy-surami-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'surami')
+    ->defaults('state', 'georgia');
+
+Route::get('/tbilisi/quran-academy-tbilisi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tbilisi')
+    ->defaults('state', 'georgia');
+
+Route::get('/telavi/quran-academy-telavi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'telavi')
+    ->defaults('state', 'georgia');
+
+Route::get('/tetritsqalos-munitsipaliteti/quran-academy-tetritsqalos-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tetritsqalos-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/tqibuli/quran-academy-tqibuli-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tqibuli')
+    ->defaults('state', 'georgia');
+
+Route::get('/tqvarcheli/quran-academy-tqvarcheli-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tqvarcheli')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsaghveri/quran-academy-tsaghveri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsaghveri')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsalenjikha/quran-academy-tsalenjikha-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsalenjikha')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsalka/quran-academy-tsalka-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsalka')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsikhisdziri/quran-academy-tsikhisdziri-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsikhisdziri')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsinandali/quran-academy-tsinandali-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsinandali')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsnori/quran-academy-tsnori-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsnori')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsqaltubo/quran-academy-tsqaltubo-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsqaltubo')
+    ->defaults('state', 'georgia');
+
+Route::get('/tsalkis-munitsipaliteti/quran-academy-tsalkis-munitsipaliteti-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tsalkis-munitsipaliteti')
+    ->defaults('state', 'georgia');
+
+Route::get('/tskhinvali/quran-academy-tskhinvali-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tskhinvali')
+    ->defaults('state', 'georgia');
+
+Route::get('/tetri-tsqaro/quran-academy-tetri-tsqaro-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'tetri-tsqaro')
+    ->defaults('state', 'georgia');
+
+Route::get('/ureki/quran-academy-ureki-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'ureki')
+    ->defaults('state', 'georgia');
+
+Route::get('/vale/quran-academy-vale-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'vale')
+    ->defaults('state', 'georgia');
+
+Route::get('/vani/quran-academy-vani-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'vani')
+    ->defaults('state', 'georgia');
+
+Route::get('/zestaponi/quran-academy-zestaponi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'zestaponi')
+    ->defaults('state', 'georgia');
+
+Route::get('/zhinvali/quran-academy-zhinvali-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'zhinvali')
+    ->defaults('state', 'georgia');
+
+Route::get('/zugdidi/quran-academy-zugdidi-georgia', [HomeController::class, 'cityPage'])
+    ->defaults('city', 'zugdidi')
+    ->defaults('state', 'georgia');
